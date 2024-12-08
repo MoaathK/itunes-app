61a2b05d8e7c9dfdfaf1be1f2dacdfb2af50b4e1
Moaath Khalid <MoathKAlahmadi@outlook.com>
2024-12-08 16:48:27 +0300

    modification on searchPage to call the Itunes API.

--- app/components/SearchPage.js
+++ app/components/SearchPage.js
@@ -8,31 +8,32 @@ const SearchPage = () => {
 
   const handleSearch = async (searchValue) => {
     try {
-      
-      const response = await fetch(`http://localhost:7036/findResults/${encodeURIComponent(searchValue)}`);  
+      // Fetch data from iTunes API for podcasts
+      const response = await fetch(`https://itunes.apple.com/search?term=${encodeURIComponent(searchValue)}&media=podcast`);  
       const data = await response.json();  
-      setSearchResults(data);  
+      setSearchResults(data.results); // iTunes API returns results in the "results" key
     } catch (error) {
       console.error("Error during search:", error); 
     }
   };
+
   return (
     <>
     <div className='flex flex-col h-screen w-screen'>
       <SearchBar onSearch={handleSearch} /> 
       <div className="flex flex-wrap gap-6 p-4 overflow-y-auto">  
         {searchResults.map((result, index) => (
-            <Link href={result.collectionViewUrl} key={index} passHref>  
-            <a className="flex flex-col items-center">  
-              <img src={result.artworkUrl600} 
-                   alt={result.artistName} className="w-32 h-32 object-cover rounded-lg" /> 
-              <div className="text-white mt-2 text-center w-36">  
-              <p >  
-                {result.collectionName}  
-              </p>
-            </div>
-            </a>
-          </Link>
+            <Link href={result.collectionViewUrl} key={index}>
+              <div className="flex flex-col items-center cursor-pointer">  
+                <img src={result.artworkUrl100} 
+                     alt={result.artistName} className="w-32 h-32 object-cover rounded-lg" /> 
+                <div className="text-white mt-2 text-center w-36">  
+                  <p>  
+                    {result.collectionName}  
+                  </p>
+                </div>
+              </div>
+            </Link>
         ))}
       </div>
     </div>
